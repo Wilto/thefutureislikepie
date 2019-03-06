b00725250faf2601f891a27ba7dc054191040d9e
Mat Marquis <mat@matmarquis.com>
2019-03-06 09:46:13 -0500

    blast my cache

--- _src/sw.js
+++ _src/sw.js
@@ -11,7 +11,7 @@ https://www.filamentgroup.com/
 (function() {
 	"use strict";
 
-	const version = "0";
+	const version = "3";
 	const cacheName = version + "::futureislikepie:";
 
 	const staticCacheName = cacheName + "static";

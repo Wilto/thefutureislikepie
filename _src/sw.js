3abf74e23acb89554ee73af5587312b51d92c80c
Mat Marquis <mat@matmarquis.com>
2019-03-27 12:40:22 -0400

    Blog archive link.

--- _src/sw.js
+++ _src/sw.js
@@ -11,7 +11,7 @@ https://www.filamentgroup.com/
 (function() {
 	"use strict";
 
-	const version = "3";
+	const version = "7";
 	const cacheName = version + "::futureislikepie:";
 
 	const staticCacheName = cacheName + "static";
